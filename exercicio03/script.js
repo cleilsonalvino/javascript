0cb295d65fed2a510f0b52c9b1a3a454cb521e40
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2023-07-27 21:42:04 -0300

    ex003

--- exercicio03/script.js
+++ exercicio03/script.js
@@ -1,17 +1,32 @@
 function contar () {
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
-    var res = document.getElementById('res')
-    
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let res = document.getElementById('res')
 
-    if (inicio.value.length == 0) {
-        res.innerHTML = 'Contagem inválida!'
-    } else if (passo.value.length == 0 ) {
-        alert('Passo inválido! Considerando PASSO 1')
-    } else if (fim.value.length == 0){
-        alert('Contagem inválida!')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
     } else {
-        for (inicio; )
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p == 0) {
+            alert('Passo inválido! Alterando para PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+           for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}` 
+           }
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) 
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+
+        res.innerHTML += `\u{1F3C1}`
+        }
+        
     }
-}
