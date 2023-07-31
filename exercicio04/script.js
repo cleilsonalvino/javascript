4dae6a35789841e21d50691d063160ea5608d1f6
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2023-07-30 22:56:46 -0300

    tabuada terminada

--- exercicio04/script.js
+++ exercicio04/script.js
@@ -1,2 +1,19 @@
-var num = document.getElementById('numero')
-var tabuada = document.getElementById('tabuada')
+function gerarTabuada() {
+    let num = document.getElementById('numero')
+    let tabuada = document.getElementById('tabuada')
+
+    if (num.value.length == 0 ) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tabuada.innerHTML = ''
+        for ( let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            tabuada.appendChild(item)
+        }
+
+    }
+
+
+}
