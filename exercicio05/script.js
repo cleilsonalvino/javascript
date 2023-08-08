298ec6d3a419fb94ce5c1fc0080fe6a961c385ff
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2023-08-08 19:40:18 -0300

    terminei o curso

--- exercicio05/script.js
+++ exercicio05/script.js
@@ -1,5 +1,5 @@
 let num = document.querySelector('input#fnum')
-let lista = document.querySelector('selec#flista')
+let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
@@ -20,10 +20,45 @@ function inLista(n, l) {
     }
 }
 
-function adicionar() {
+function adicionar() { 
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        alert('Tudo OK!')
+        valores.push(Number(num.value))
+        let item = document.createElement('option') //criou um option
+        item.text = `Valor ${num.value} adicionado!`
+        lista.appendChild(item) //adicionou o option no select
+        res.innerHTML = '' // limpa o res
     } else {
         alert('Valor inválido ou já encontrado na lista!')
+    }
+    num.value = '' //deixa a parte que coloca o numero vazia
+    num.focus() //faz o cursor picar para adicionar um texto ou numero
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores para finalizar!')
+    } else {
+        let total = valores.length // conta os valores adicionados
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos < menor]) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadrastados.</p>`
+        res.innerHTML += `<p>O maior número adicionado foi o ${maior}</p>`
+        res.innerHTML += `<p>O menor número adicionado foi o ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é de ${media}</p>`
+        
     }
 }
